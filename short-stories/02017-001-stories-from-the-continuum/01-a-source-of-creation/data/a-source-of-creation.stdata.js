67956177a0e46656af6512ec83cf59a5e59deda4
Michael Axonn <axonn.echysttas.wr@gmail.com>
2017-04-20 16:35:37 +0200

    Made progress with 03.stseg. Added author information to stdata files.

--- short-stories/02017-001-stories-from-the-continuum/01-a-source-of-creation/data/a-source-of-creation.stdata.js
+++ short-stories/02017-001-stories-from-the-continuum/01-a-source-of-creation/data/a-source-of-creation.stdata.js
@@ -1,12 +1,13 @@
 var SS_01_SFTC_ASourceOfCreation_TOC = [];
 
-SS_01_SFTC_ASourceOfCreation_TOC.push( { file: "data/01.stseg", rawDescription: "1..."} );
-SS_01_SFTC_ASourceOfCreation_TOC.push( { file: "data/02.stseg", rawDescription: "...1"} );
+SS_01_SFTC_ASourceOfCreation_TOC.push( { file: "data/01.stseg", rawDescription: "1...", authors: "Michael Axonn"} );
+SS_01_SFTC_ASourceOfCreation_TOC.push( { file: "data/02.stseg", rawDescription: "...1", authors: "Michael Axonn"} );
 
 var SS_01_SFTC_ASourceOfCreation_Data =
 		{
 			baseURL: "short-stories/02017-001-stories-from-the-continuum/" +
 			"01-a-source-of-creation/",
+			curator: "Michael Axonn",
 			title: "A Source of Creation",
 			toc: SS_01_SFTC_ASourceOfCreation_TOC
 		};
--- short-stories/02017-001-stories-from-the-continuum/02-jonas-is-flying/data/jonas-is-flying.stdata.js
+++ short-stories/02017-001-stories-from-the-continuum/02-jonas-is-flying/data/jonas-is-flying.stdata.js
@@ -1,14 +1,15 @@
 var SS_01_SFTC_JonasIsFlying_TOC = [];
 
-SS_01_SFTC_JonasIsFlying_TOC.push( { file: "data/01.stseg", rawDescription: "1"} );
-SS_01_SFTC_JonasIsFlying_TOC.push( { file: "data/02.stseg", rawDescription: "2"} );
-SS_01_SFTC_JonasIsFlying_TOC.push( { file: "data/03.stseg", rawDescription: "3"} );
-SS_01_SFTC_JonasIsFlying_TOC.push( { file: "data/04.stseg", rawDescription: "4"} );
+SS_01_SFTC_JonasIsFlying_TOC.push( { file: "data/01.stseg", rawDescription: "1", authors: "Michael Axonn"} );
+SS_01_SFTC_JonasIsFlying_TOC.push( { file: "data/02.stseg", rawDescription: "2", authors: "Michael Axonn"} );
+SS_01_SFTC_JonasIsFlying_TOC.push( { file: "data/03.stseg", rawDescription: "3", authors: "Michael Axonn"} );
+SS_01_SFTC_JonasIsFlying_TOC.push( { file: "data/04.stseg", rawDescription: "4", authors: "Michael Axonn"} );
 
 var SS_01_SFTC_JonasIsFlying_Data =
 		{
 			baseURL: "short-stories/02017-001-stories-from-the-continuum/" +
 			"02-jonas-is-flying/",
+			curator: "Michael Axonn",
 			title: "Jonas Is Flying",
 			toc: SS_01_SFTC_JonasIsFlying_TOC
 		};
