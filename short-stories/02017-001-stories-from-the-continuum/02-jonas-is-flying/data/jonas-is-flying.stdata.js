f58c13e9d1ff5cbc8202117d069777d09cfd27d1
Michael Axonn <axonn.echysttas.wr@gmail.com>
2017-04-29 01:47:03 +0200

    More story reorg. 06.stseg @ draft 1. Proceeding with 07.stseg.

--- short-stories/02017-001-stories-from-the-continuum/02-jonas-is-flying/data/jonas-is-flying.stdata.js
+++ short-stories/02017-001-stories-from-the-continuum/02-jonas-is-flying/data/jonas-is-flying.stdata.js
@@ -4,6 +4,9 @@ SS_01_SFTC_JonasIsFlying_TOC.push( { file: "data/01.stseg", rawDescription: "1",
 SS_01_SFTC_JonasIsFlying_TOC.push( { file: "data/02.stseg", rawDescription: "2", authors: "Michael Axonn"} );
 SS_01_SFTC_JonasIsFlying_TOC.push( { file: "data/03.stseg", rawDescription: "3", authors: "Michael Axonn"} );
 SS_01_SFTC_JonasIsFlying_TOC.push( { file: "data/04.stseg", rawDescription: "4", authors: "Michael Axonn"} );
+SS_01_SFTC_JonasIsFlying_TOC.push( { file: "data/05.stseg", rawDescription: "5", authors: "Michael Axonn"} );
+SS_01_SFTC_JonasIsFlying_TOC.push( { file: "data/06.stseg", rawDescription: "6", authors: "Michael Axonn"} );
+SS_01_SFTC_JonasIsFlying_TOC.push( { file: "data/07.stseg", rawDescription: "7", authors: "Michael Axonn"} );
 
 var SS_01_SFTC_JonasIsFlying_Data =
 		{
